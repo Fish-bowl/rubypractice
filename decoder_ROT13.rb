3c9f5ed44a4911af7aa402ec022994e5a9057f3b
Fish-bowl <symbolt.ap@gmail.com>
2019-01-30 18:19:46 -0700

    more cypher work...getting closer

--- decoder_ROT13.rb
+++ decoder_ROT13.rb
@@ -1,44 +1,33 @@
 require 'pry'
 
-secret_messages = ["Jul qvq gur puvpxra pebff gur ebnq?", "Gb trg gb gur bgure fvqr!"]
+# def cypher 
+#   secret_messages = ["Jul qvq gur puvpxra pebff gur ebnq?", "this is also a secret. shhh"]
+#   secret_messages.each do |message|
+#     ascii = message.chars.map { |c| c.ord}
+#     decyphered = ascii.map { |c| c + 13 }
+#     x = decyphered.map { |c| c.chr }.join
+#     puts x 
+#   end
+# end 
 
-def rot13(secret_messages)
-  alphabet = ("a".."z").to_a.join
-  c_alphabet = ("a".."z").to_a.rotate(13).join
-  a = (a..z)
-  b = (n..z, a..m)
-  # alphabet_hash = alphabet.to_h
-  # c_alphabet_hash = c_alphabet.each_line {|num, index|}
-  # @arr = []
-  # secret_messages.map { |message| 
-  #   x = message.chars 
-  #   x.each do |m|
-  #     m
-  #   end
-  #   binding.pry
-  # } #@arr << message.chars.rotate(13).to_a }
-  # puts @arr
-  binding.pry
+# cypher
 
-  secret_messages.each_with_object({}){ |h, k| h[k]}
+ALPHABET_SIZE = 26
+string = ["Jul qvq gur puvpxra pebff gur ebnq?", "this is also a secret. shhh"]
+
+def caesar_cipher(string)
+  shiftyArray = []
+  string.each do |m|
+    charLine = m.chars.map(&:ord)
+    shift = 13
+      shiftyArray << charLine.map do |c|
+        puts c 
+        (c + shift).chr
+        # ((c + shift) < 123 ? (c + shift) : (c + shift) - 26).chr
+        # ((c + shift)  )
+      end.join
+    end
+  puts shiftyArray
 end
 
-rot13(secret_messages)
-
-# %w{a b c}.reduce({}){|a,v| a[v] = v.upcase; a}
-
-# class Caesar
-#   def initialize(shift, alphabet = ('a'..'z').to_a.join)
-#     i = shift % alphabet.size #I like this
-#     @decrypt = alphabet
-#     @encrypt = alphabet[i..-1] + alphabet[0...i]
-#   end
-
-#   def encrypt(string)
-#     string.tr(@decrypt, @encrypt)
-#   end
-
-#   def decrypt(string)
-#     string.tr(@encrypt, @decrypt)
-#   end
-# end
+puts caesar_cipher(string)
