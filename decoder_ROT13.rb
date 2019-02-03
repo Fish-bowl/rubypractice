0739160d16020e0177b0f8a04c33ee53a86a465d
Fish-bowl <symbolt.ap@gmail.com>
2019-02-03 15:10:52 -0700

    It finnaly works

--- decoder_ROT13.rb
+++ decoder_ROT13.rb
@@ -1,33 +1,8 @@
 require 'pry'
+messages = ["Jul qvq gur puvpxra pebff gur ebnq?", "this is also a string" ]
 
-# def cypher 
-#   secret_messages = ["Jul qvq gur puvpxra pebff gur ebnq?", "this is also a secret. shhh"]
-#   secret_messages.each do |message|
-#     ascii = message.chars.map { |c| c.ord}
-#     decyphered = ascii.map { |c| c + 13 }
-#     x = decyphered.map { |c| c.chr }.join
-#     puts x 
-#   end
-# end 
-
-# cypher
-
-ALPHABET_SIZE = 26
-string = ["Jul qvq gur puvpxra pebff gur ebnq?", "this is also a secret. shhh"]
-
-def caesar_cipher(string)
-  shiftyArray = []
-  string.each do |m|
-    charLine = m.chars.map(&:ord)
-    shift = 13
-      shiftyArray << charLine.map do |c|
-        puts c 
-        (c + shift).chr
-        # ((c + shift) < 123 ? (c + shift) : (c + shift) - 26).chr
-        # ((c + shift)  )
-      end.join
-    end
-  puts shiftyArray
+def cypher(messages)
+  messages.map { |m| puts m.tr 'A-Za-z', 'N-ZA-Mn-za-m'  }
 end
 
-puts caesar_cipher(string)
+cypher(messages)
