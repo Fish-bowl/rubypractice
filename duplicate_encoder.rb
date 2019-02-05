bce36cf767bcb580a6df48f7b9584b4aebd6800b
Fish-bowl <symbolt.ap@gmail.com>
2019-02-04 19:17:36 -0700

    fuck these enumerables man...im really trying to understand man...

--- duplicate_encoder.rb
+++ duplicate_encoder.rb
@@ -1,10 +1,11 @@
 require 'pry'
 word = "thississit"
 
-def duplicate_encode(word)
-  word.chars do |c|
-    
+def duplicate_encoder(word)
+  binding.pry 
+  word.split("") do |c|
+   puts c
   end
 end
 
-duplicate_encode(word)
+duplicate_encoder(word)
---
+++ test.rb
@@ -0,0 +1,12 @@
+puts "enter the first number: "
+num1 = gets.strip
+puts "enter the operator: "
+op = gets.strip
+puts "enter the final number: " 
+num2 = gets.strip
+
+def calc(num1, num2, op)
+	if op == '-' 
+	puts "#{num1} #{op} #{num2} = sum"
+end
+
